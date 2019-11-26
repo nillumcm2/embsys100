f4c906b712555b679f6144bda72a32f68640e950
nillumcm2 <56521824+nillumcm2@users.noreply.github.com>
2019-11-25 17:51:07 -0800

    Add files via upload

---
+++ assignment05/swap/Swap.h
@@ -0,0 +1,11 @@
+#ifndef __SWAP_H__
+#define __SWAP_H__
+
+
+
+void Swap(int** holdOne, int** holdTwo);
+
+
+
+
+#endif
---
+++ assignment05/swap/main.c
@@ -0,0 +1,31 @@
+// This program is used to swap two pointers
+#include "Swap.h"
+
+int main()
+{
+  
+
+  int x = 5;
+  int y = 10;
+  
+  int *ptr_x = &x;
+  int *ptr_y = &y;
+  
+  Swap(&ptr_y, &ptr_x );
+  
+  
+  
+  
+  
+  
+  
+  //Swap(&ptr_y, &ptr_x );
+  
+  
+  while(1)
+  {
+    
+    
+  }
+  return 0;
+}
